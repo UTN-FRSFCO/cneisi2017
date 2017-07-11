f4f3a0acd526a0a74722a03da7a17264169a0108
Lautaro Lubatti <lr.lubatti@gmail.com>
2017-07-11 11:35:18 -0300

    Fix g+ client

--- config/services.php
+++ config/services.php
@@ -48,7 +48,7 @@ return [
     ],
 
     'google' => [
-        'client_id' => '616276544246-erk7iapvmb419e5s850fa25p7arb3vo5.apps.googleusercontent.com',
+        'client_id' => '616276544246-lsfhg36ebfd4lcciev02hjqh0mrt412q.apps.googleusercontent.com',
         'client_secret' => env('GOOGLE_SECRET'),
         'redirect' => 'http://cneisi.sanfrancisco.utn.edu.ar/callback/google',
     ],
