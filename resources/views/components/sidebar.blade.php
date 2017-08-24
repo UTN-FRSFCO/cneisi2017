a92bb08800819af5c6985b4ddc2184a1b658e79a
Lautaro Lubatti <lr.lubatti@gmail.com>
2017-08-24 16:46:25 -0300

    add gallardo img

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -8,8 +8,8 @@
                 <h6 class="align-center" style="margin-top: 20px;"><span class="highlight" style="color: rgba(254, 73, 24, 0.85);">{{ trans('strings.call-sponsors') }}</span></h6>
                 <p class="align-center"> Si estás interesado en apadrinar las actividades del congreso, ponete en contacto con nosotros mediante la siguiente dirección de correo: <a href="">cneisi@sanfrancisco.utn.edu.ar</a> </p>
                 <h6 style="margin-top: 20px"><span class="highlight" style="color: rgba(254, 73, 24, 0.85);">{{ trans('strings.follow-us') }}</span></h6>
-                <p><a href="{{ config('constants.FACEBOOK_LINK') }}" target="_blank"><i class="fa fa-facebook-f fa-2x"></i></a></p>
-                <p><a href="{{ config('constants.TWITTER_LINK') }}" target="_blank"><i class="fa fa-twitter fa-2x"></i></a><br></p>
+                <p><a href="{{ config('constants.FACEBOOK_LINK') }}" target="_blank"><i class="fa fa-facebook-f fa-2x" style="margin-right: 5px;"></i></a>
+                <a href="{{ config('constants.TWITTER_LINK') }}" target="_blank"style="margin-left: 5px;"><i class="fa fa-twitter fa-2x"></i></a><br></p>
                 <a href="{{ config('constants.SITE_LINK') }}" target="_blank"><img src="{{asset('/img/logo_utn_web.png')}}" alt="utn_web_logo" class="img-responsive center-block" /></a><br>
             </div>
         </div>
