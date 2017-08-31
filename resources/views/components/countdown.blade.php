61a800c25adb0604af6fe002ee639b877161bb93
Lautaro Lubatti <lr.lubatti@gmail.com>
2017-08-31 00:19:03 -0300

    countdown

--- resources/views/components/countdown.blade.php
+++ resources/views/components/countdown.blade.php
@@ -1,57 +1,8 @@
 <section class="section align-center">
     <div class="container">
 
-        <!-- Countdown start -->
-        <div class="row counters-wrapper countdown">
-            <div class="col-sm-3">
-                <div class="counter-block counter-block-no-border has-separator">
-                    <div class="counter-box">
-                        <div class="counter-content">
-                            <span class="count">{dnn}</span>
-
-                            <p class="title">días</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-3">
-                <div class="counter-block counter-block-no-border has-separator">
-                    <div class="counter-box">
-                        <div class="counter-content">
-                            <span class="count">{hnn}</span>
-
-                            <p class="title">horas</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-3">
-                <div class="counter-block counter-block-no-border has-separator">
-                    <div class="counter-box">
-                        <div class="counter-content">
-                            <span class="count">{mnn}</span>
-
-                            <p class="title">minutos</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-3">
-                <div class="counter-block counter-block-no-border">
-                    <div class="counter-box">
-                        <div class="counter-content">
-                            <span class="count">{snn}</span>
-
-                            <p class="title">segundos</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
+        <h1 style="color: #1ac6ff"> Ya comenzó el congreso!</h1>
+        <h5>Subí tus fotos con el hashtag #cneisi2017 y podrá ser publicada!</h5>
 
     </div>
 </section>
--- resources/views/partials/about.blade.php
+++ resources/views/partials/about.blade.php
@@ -9,10 +9,10 @@
         <div class="tabs-wrapper tabs-horizontal">
             <ul class="nav nav-tabs">
                 <li><a href="#horizontal_tab1" data-toggle="tab">
-                        <h6 class="heading-alt"><span class="fa fa-code"></span> Información general</h6>
+                        <h6 class="heading-alt"><span class="fa fa-code"></span> Cuenta regresiva</h6>
                     </a></li>
                 <li class="active"><a href="#horizontal_tab2" data-toggle="tab">
-                        <h6 class="heading-alt"><span class="fa fa-clock-o"></span> Cuenta regresiva</h6>
+                        <h6 class="heading-alt"><span class="fa fa-clock-o"></span> Información general</h6>
                     </a></li>
                 <li><a href="#horizontal_tab3" data-toggle="tab">
                         <h6 class="heading-alt"><span class="fa fa-external-link"></span> Sponsors</h6>
@@ -23,26 +23,25 @@
 
                 <div id="horizontal_tab1" class="tab-pane fade">
 
-                    <div class="col-sm-5 img-column">
-                        <img src="{{asset('/img/utn-entrance.jpg')}}" alt="utn_logo" class="img-responsive" />
-                    </div>
-                    <div class="col-sm-7 align-left" style="padding-top: 20px;">
-                        <h6>Acerca del evento</h6>
-                        <p>La facultad regional San Francisco de la <strong>UTN</strong> presenta el undécimo congreso nacional de Ingeniería en Sistemas, cuya realización busca reunir a todas las delegaciones de las distintas facultades regionales y público en general para poder otorgar un espacio de capacitación, reunión, comunión, difusión a través de las diferentes disertaciones realizadas parte de profesionales, graduados y estudiantes vinculados tecnologías de información y sistemas informáticos; y también propiciar un espacio para la presentación y difusión de trabajos realizados en las diferentes regionales sobre investigación científica, desarrollo y/o aplicaciónes innovadoras relacionadas con el rubro.</p>
-                        <p>El evento se llevará a cabo el 31 de agosto y 1 de septiembre en las instalaciones de la facultad. Nuestro objetivo es poder lograr y favorecer el intercambio académico y cultural que año a año nos permite acercarnos y compartir entre todos esta fructífera experiencia.</p>
-                        <div class="align-center" style="margin-top: 15px;">
-                            <a href="{{ route('about') }}" class="btn btn-outline-clr btn-sm ">{{ trans('buttons.see-more') }} </a>
-                            @include('components.share', ['url' => 'http://cneisi.sanfrancisco.utn.edu.ar/#acerca-de' ])
-                        </div>
-                    </div>
-
+                    @include('components.countdown')
 
                 </div>
 
 
                     <div id="horizontal_tab2" class="tab-pane fade active in">
 
-                        @include('components.countdown')
+                        <div class="col-sm-5 img-column">
+                            <img src="{{asset('/img/utn-entrance.jpg')}}" alt="utn_logo" class="img-responsive" />
+                        </div>
+                        <div class="col-sm-7 align-left" style="padding-top: 20px;">
+                            <h6>Acerca del evento</h6>
+                            <p>La facultad regional San Francisco de la <strong>UTN</strong> presenta el undécimo congreso nacional de Ingeniería en Sistemas, cuya realización busca reunir a todas las delegaciones de las distintas facultades regionales y público en general para poder otorgar un espacio de capacitación, reunión, comunión, difusión a través de las diferentes disertaciones realizadas parte de profesionales, graduados y estudiantes vinculados tecnologías de información y sistemas informáticos; y también propiciar un espacio para la presentación y difusión de trabajos realizados en las diferentes regionales sobre investigación científica, desarrollo y/o aplicaciónes innovadoras relacionadas con el rubro.</p>
+                            <p>El evento se llevará a cabo el 31 de agosto y 1 de septiembre en las instalaciones de la facultad. Nuestro objetivo es poder lograr y favorecer el intercambio académico y cultural que año a año nos permite acercarnos y compartir entre todos esta fructífera experiencia.</p>
+                            <div class="align-center" style="margin-top: 15px;">
+                                <a href="{{ route('about') }}" class="btn btn-outline-clr btn-sm ">{{ trans('buttons.see-more') }} </a>
+                                @include('components.share', ['url' => 'http://cneisi.sanfrancisco.utn.edu.ar/#acerca-de' ])
+                            </div>
+                        </div>
 
                     </div>
 
